0c3a46f82a7b68dc268ce5855e236bb547e425e1
iKinsure <59565620+iKinsure@users.noreply.github.com>
2021-01-20 15:12:03 +0100

    refactoring

--- project.ino
+++ project.ino
@@ -17,13 +17,13 @@ static const int RX_PIN = 12;
 
 // standardowe stany dzialania
 static const int POSITION = 0; 
-static const int DATETIME = 1; 
-static const int DISTANCE = 2; 
-static const int ALTITUDE = 3; 
-static const int NAVIGATION = 4; 
-static const int SIGNAL = 5;
-static const int SPEED = 6;
-static const int DESTINATION = 7;
+static const int SPEED = 1;
+static const int ALTITUDE = 2; 
+static const int DESTINATION = 3;
+static const int DISTANCE = 4; 
+static const int NAVIGATION = 5; 
+static const int DATETIME = 6; 
+static const int SIGNAL = 7;
 
 // niestandardowe stany dzialania
 static const int CLICK = 8; 
@@ -31,9 +31,9 @@ static const int ACCEPT = 9;
 static const int ERROR = 10; 
 
 // inicjacja zmiennych
-static volatile int current_mode = POSITION;
-static volatile int prev_mode = POSITION;
-static volatile int prev_helper_mode = POSITION;
+static volatile int current_mode = 0;
+static volatile int prev_mode = 0;
+static volatile int prev_helper_mode = 0;
 
 // miejsce dla ustawien w EEPROM
 static const int EE_LATITUDE = EEPROM.getAddress(sizeof(double));
@@ -42,6 +42,7 @@ static const int EE_LIGHT = EEPROM.getAddress(sizeof(bool));
 static const int EE_ALT_VAR = EEPROM.getAddress(sizeof(int));
 static const int EE_SPEED_VAR = EEPROM.getAddress(sizeof(int));
 static const int EE_NAVIGATE = EEPROM.getAddress(sizeof(bool));
+static const int EE_DIST_VAR = EEPROM.getAddress(sizeof(int));
 
 // ustawienia
 double hLatitude = 0.0;
@@ -50,6 +51,7 @@ bool light_on = false;
 int alt_var = 0;
 int speed_var = 0;
 bool navigate = false;
+int dist_var = 0;
 
 unsigned long interrupt_time = 0;
 LiquidCrystal lcd(4, 5, 6, 7, 8, 9);
@@ -98,39 +100,39 @@ void loop() {
 
         // glowne stany
         switch (current_mode) { 
-         case POSITION:
-          gps_pos();
-          break;
-         case DATETIME:
-          gps_datetime();
-          break;
-         case DISTANCE:
-          gps_distance();
-          break;
+         case POSITION: // standardowe
+            gps_position();
+            break;
+         case SPEED:
+            gps_speed();
+            break;
          case ALTITUDE:
-          gps_altitude();
-          break;
+            gps_altitude();
+            break;
+         case DESTINATION:
+            gps_destination();
+            break;
+         case DISTANCE:
+            gps_distance();
+            break;
          case NAVIGATION:
-          gps_navigate();
-          break;
+            gps_navigation();
+            break;
+         case DATETIME:
+            gps_datetime();
+            break;
         case SIGNAL:
-          gps_signal();
-          break;
-        case SPEED:
-          gps_speed();
-          break;
-        case DESTINATION:
-          gps_destination();
-          break;
-        case CLICK:
-          click();
-          break;
+            gps_signal();
+            break;
+        case CLICK: // niestandardowe
+            click();
+            break;
         case ACCEPT:
-          accept();
-          break;
+            accept();
+            break;
         case ERROR:
-          error();
-          break;
+            error();
+            break;
        }
     }
   }
@@ -156,7 +158,7 @@ void decline_button() {
     } else if (current_mode == ERROR) {
         // brak dzialania
     } else {
-      current_mode = current_mode >= DESTINATION // ostatni standardowy tryb
+      current_mode = current_mode >= SIGNAL // ostatni standardowy tryb
                 ? POSITION  // pierwszy standardowy tryb
                 : current_mode + 1; 
     }
@@ -197,6 +199,7 @@ void click() {
       break;
     case SPEED:
     case ALTITUDE:
+    case DISTANCE:
       print(F("ZMIANA JEDNOSTKI"), F("NIE          TAK"));
       break;
     case NAVIGATION:
@@ -239,6 +242,12 @@ void accept() {
         EEPROM.writeBit(EE_NAVIGATE, 0, navigate);
       }
       break;
+    case DISTANCE:
+      dist_var = dist_var >= 3 ? 0 : dist_var + 1; 
+      if (EEPROM.isReady()) {
+        EEPROM.writeInt(EE_DIST_VAR, dist_var);
+      }
+      break;
     default:
       light_on = !light_on;
       analogWrite(LCD_LED_PIN, light_on == true ? 255 : 0);
@@ -262,9 +271,43 @@ void error() {
 /**
  * Aktualna pozycja
  */
-void gps_pos() {
+void gps_position() {
   if (gps.location.isUpdated()) {
     print("AKT " + String(gps.location.lat(), 6), "POZ " + String(gps.location.lng(), 6));
+  }
+}
+
+/**
+ * Predkosc
+ */
+void gps_speed() {
+  if (gps.speed.isUpdated()) {
+    if (speed_var == 0) {
+      print(F("PREDKOSC"), String(gps.speed.knots()) + " wezlow");
+    } else if (speed_var == 1) {
+      print(F("PREDKOSC"), String(gps.speed.mph()) + " mph");
+    } else if (speed_var == 2) {
+      print(F("PREDKOSC"), String(gps.speed.mps()) + " mps");
+    } else {
+      print(F("PREDKOSC"), String(gps.speed.kmph()) + " kmh");
+    }
+  }
+}
+
+/**
+ * Aktualna wysokosc
+ */
+void gps_altitude() {
+  if (gps.altitude.isUpdated()) {
+    if (alt_var == 0) {
+      print(F("WYSOKOSC NPM"), String(gps.altitude.meters()) + " m");
+    } else if (alt_var == 1) {
+      print(F("WYSOKOSC NPM"), String(gps.altitude.miles()) + " mil");
+    } else if (alt_var == 2) {
+      print(F("WYSOKOSC NPM"), String(gps.altitude.kilometers()) + " km");
+    } else {
+      print(F("WYSOKOSC NPM"), String(gps.altitude.feet()) + " ft");
+    }
   }
 }
 
@@ -288,26 +331,18 @@ void gps_distance() {
     double distance = TinyGPSPlus::distanceBetween(
                       gps.location.lat(), gps.location.lng(),
                       hLatitude, hLongitude);
-    print(F("ODLEGLOSC CEL"), String(distance) + " m");
-  }
-}
-
-/**
- * Aktualna wysokosc
- */
-void gps_altitude() {
-  if (gps.altitude.isUpdated()) {
-    if (alt_var == 0) {
-      print(F("WYSOKOSC NPM"), String(gps.altitude.meters()) + " m");
-    } else if (alt_var == 1) {
-      print(F("WYSOKOSC NPM"), String(gps.altitude.miles()) + " mil");
-    } else if (alt_var == 2) {
-      print(F("WYSOKOSC NPM"), String(gps.altitude.kilometers()) + " km");
+    if (dist_var == 0) {
+      print(F("ODLEGLOSC CEL"), String(distance) + " m");
+    } else if (dist_var == 1) {
+      print(F("ODLEGLOSC CEL"), String(_GPS_MILES_PER_METER  * distance) + " mil");
+    } else if (dist_var == 2) {
+      print(F("ODLEGLOSC CEL"), String(_GPS_KM_PER_METER * distance) + " km");
     } else {
-      print(F("WYSOKOSC NPM"), String(gps.altitude.feet()) + " ft");
+      print(F("ODLEGLOSC CEL"), String(_GPS_FEET_PER_METER * distance) + " ft");
     }
   }
 }
+
 
 /**
  * Ilosc satelit i sygnal
@@ -318,27 +353,12 @@ void gps_signal() {
   }
 }
 
-/**
- * Predkosc
- */
-void gps_speed() {
-  if (gps.speed.isUpdated()) {
-    if (speed_var == 0) {
-      print(F("PREDKOSC"), String(gps.speed.knots()) + " wezlow");
-    } else if (speed_var == 1) {
-      print(F("PREDKOSC"), String(gps.speed.mph()) + " mph");
-    } else if (speed_var == 2) {
-      print(F("PREDKOSC"), String(gps.speed.mps()) + " mps");
-    } else {
-      print(F("PREDKOSC"), String(gps.speed.kmph()) + " kmh");
-    }
-  }
-}
+
 
 /**
  * Nawigacja do zapisanego miejsca
  */
-void gps_navigate() {
+void gps_navigation() {
   if (gps.location.isUpdated()) {
     double course = TinyGPSPlus::courseTo(
                   gps.location.lat(), gps.location.lng(),
